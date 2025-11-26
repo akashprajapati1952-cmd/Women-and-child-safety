43c1557bbd988cfe9b66de3642c0a6e40df7628d
akashprajapati1952-cmd <akashprajapati1952@gmail.com>
2025-11-26 15:52:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,155 @@
+const blockdata={
+  agra:[
+    {value:"sadholi kadeem", label:"सढोली कदीम"}
+    ],
+  aligarh:[
+    {value:"mujaffar nagar", label:"मुजफ्फराबाद"}
+    ],
+  "ambedkar nagar":[
+    {value:"puvarka", label:"पुंवारका"}
+    ],
+  amethi:[
+    {value:"baliyakhedi",label:"बलियाखेड़ी"}
+    ],
+  amroha:[
+    { value:"nanauta",label:"नानौता"}
+    ],
+  auraiya:[
+    {value:"devband",label:"देवबंद"}
+    ],
+  aajamgarh:[
+    {value:"rampur",label:"रामपुर"}
+    ],
+  baagpat:[
+    {value:"naagal",label:"नागल"}
+    ],
+  bahraich:[
+    {value:"nakud",label:"नकुड़"}
+    ],
+  baliya:[
+    {value:"sarsanva",label:"सरसांवा"}
+    ],
+  balrampur:[
+    {value:"gangoh",label:"गंगोह"}
+    ],
+  banda:[
+    {value:"saharanpur",label:"सहारनपुर"}
+    ]
+}
+const officerList={
+  "sadholi kadeem":[
+    {name:"बेबी रानी मौय॔",type:"सरकारी अधिकारी",position:"महिला कल्याण, बाल विकास एवं पुष्टाहार मंत्री, उत्तर प्रदेश सरकार",gender:"female", photo:"https://mahilakalyan.up.nic.in/Baby_Rani_Maurya.jpeg", contact:"0522-2288532",whatsapp_contact:"9876543210",address:"निदेशालय महिला कल्याण जवाहर भवन, अशोक मार्ग लखनऊ, उत्तर प्रदेश"}
+    
+    ],
+  "mujaffar nagar":[
+    {name:"श्रीमती प्रतिभा शुक्ला",type:"सरकारी अधिकारी", position:"राज्य मंत्री, महिला कल्याण, बाल विकास एवं पुष्टाहार मंत्री, उत्तर प्रदेश सरकार", gender:"female", photo:"https://mahilakalyan.up.nic.in/PrtibhaShukla.png", contact:"9876543211",whatsapp_contact:"9876543211", address:"ग्राम पंचायत कार्यालय, मुजफ्फराबाद, सहारनपुर"}
+    ],
+  puvarka:[
+    {name:"सुनीता रानी", type:"सरकारी अधिकारी	", position:"सरकारी अधिकारी	", gender:"Female", photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnNj1A8jWVRvxws_Zw3Rn6KYDzEYIqtINtYQ&s", contact:"9876543212", whatsapp_contact:"9876543212", address:"पंचायत भवन, पुवरका, सहारनपुर"}
+    ],
+  baliyakhedi:[
+    {name:"राहुल अग्रवाल", type:"सामाजिक कार्यकर्ता", position:"सामुदायिक सहयोगी (सामाजिक)", gender:"Male", photo:"https://media.istockphoto.com/id/1356281872/vector/happy-people-on-the-hand-care-logo.jpg?s=612x612&w=0&k=20&c=ebVD4uTDg-h6pta7AbJEVki32E_gVwsK1AkxvwjBXMA=", contact:"9876543213", whatsapp_contact:"9876543213", address:"पंचायत भवन, बलिया खेडी, सहारनपुर"}
+    ],
+  nanauta:[
+    {name:"	रचना पांडे", type:"	सरकारी अधिकारी", position:"सुरक्षा प्रतिक्रिया समन्वयक (सरकारी)", gender:"Female", photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnNj1A8jWVRvxws_Zw3Rn6KYDzEYIqtINtYQ&s", contact:"9876543214", whatsapp_contact:"9876543214", address:"तहसील परिसर, ननौता, सहारनपुर"}
+    ],
+  devband:[
+    {name:"निधि टंडन", type:"सरकारी अधिकारी", position:"महिला सुरक्षा अधिकारी (सरकारी)", gender:"Female", photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnNj1A8jWVRvxws_Zw3Rn6KYDzEYIqtINtYQ&s", contact:"9876543215", whatsapp_contact:"9876543215", address:"ब्लॉक ऑफिस, देवबंद, सहारनपुर"}
+    ],
+  rampur:[
+    {name:"अरुणा ठाकुर", type:"सामाजिक कार्यकर्ता", position:"महिला हेल्पलाइन समन्वयक (सामाजिक)", gender:"Female", photo:"https://media.istockphoto.com/id/1356281872/vector/happy-people-on-the-hand-care-logo.jpg?s=612x612&w=0&k=20&c=ebVD4uTDg-h6pta7AbJEVki32E_gVwsK1AkxvwjBXMA=", contact:"9876543216", whatsapp_contact:"9876543216", address:"ग्राम पंचायत कार्यालय, रामपुर मनिहारन, सहारनपुर	"}
+    ],
+  naagal:[
+    {name:"श्वेता सक्सेना", type:"NGO", position:"महिला अधिकार कार्यकर्ता (NGO)", gender:"Female", photo:"https://www.shutterstock.com/image-vector/ngo-earth-planet-hands-palm-260nw-1867730668.jpg", contact:"9876543217", whatsapp_contact:"9876543217", address:"ब्लॉक ऑफिस, नागल, सहारनपुर"}
+    ],
+  nakud:[
+    {name:"पूजा शर्मा", type:"NGO", position:"महिला सशक्तिकरण कार्यकर्ता (NGO)", gender:"Female", photo:"https://www.shutterstock.com/image-vector/ngo-earth-planet-hands-palm-260nw-1867730668.jpg", contact:"9876543218", whatsapp_contact:"9876543218", address:"पुलिस चौकी, नकुड़, सहारनपुर"}
+    ],
+  sarsanva:[
+    {name:"अनिल यादव", type:"सरकारी अधिकारी", position:"जिला सुरक्षा अधिकारी (सरकारी)", gender:"Male", photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnNj1A8jWVRvxws_Zw3Rn6KYDzEYIqtINtYQ&s", contact:"9876543219", whatsapp_contact:"9876543219", address:"ब्लॉक ऑफिस, सरसावा, सहारनपुर	"}
+    ],
+  gangoh:[
+    {name:"रितेश सिंह", type:"NGO", position:"सामाजिक कार्यकर्ता (NGO)", gender:"Male", photo:"https://www.shutterstock.com/image-vector/ngo-earth-planet-hands-palm-260nw-1867730668.jpg", contact:"9876543220", whatsapp_contact:"9876543220", address:"तहसील परिसर, गंगोह, सहारनपुर"}
+    ],
+  saharanpur:[
+    {name:"राजीव चौधरी", type:"सरकारी अधिकारी", position:"जिला संरक्षण अधिकारी (सरकारी)", gender:"Male", photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnNj1A8jWVRvxws_Zw3Rn6KYDzEYIqtINtYQ&s", contact:"9876543221", whatsapp_contact:"9876543221", address:"जिला कार्यालय, सहारनपुर"}
+    ]
+}
+let blk="";
+const home=document.getElementById('content').innerHTML;
+document.getElementById('home').addEventListener('click',function(){
+  location.reload();
+  });
+  
+const blocklist=function(){
+  const district=this.value;
+  console.log(district)
+  const selectBlock=document.getElementById("block");
+  selectBlock.innerHTML=`<option disabled selected value="" >अपना ब्लाॅक चुनें।</option>`
+  if(blockdata[district]){
+    blockdata[district].forEach(function(dist){
+      const opt=document.createElement("option");
+      opt.textContent=dist.label;
+      opt.value=dist.value;
+      selectBlock.appendChild(opt);
+    })
+  }}
+document.getElementById('district').addEventListener('change',blocklist);
+document.getElementById('block').addEventListener('change',function(){
+  blk=this.value;
+  console.log(blk)
+});
+const details=function(){
+  if(officerList[blk]){
+    let div=document.getElementById("content");
+    div.innerHTML="";
+    div.id="content"
+    officerList[blk].forEach(function(officer){
+      const mydiv=document.createElement("div");
+      mydiv.id="cardBody"
+      const newdiv=document.createElement("div");
+      newdiv.classList.add('officerDetails');
+      const pic=document.createElement("img");
+      const name1=document.createElement("p");
+      const gender1=document.createElement("p");
+      const type1=document.createElement("p");
+      const position1=document.createElement("p");
+      const contact1=document.createElement("p");
+      const whatsapp_contact1=document.createElement("p");
+      const address1=document.createElement("p");
+      const newdiv1=document.createElement("div");
+      pic.src=officer.photo;
+      pic.alt="officer pic";
+      name1.innerHTML='Name: '+officer.name;
+      name1.classList.add('name');
+      gender1.innerHTML='Gender: '+officer.gender;
+      type1.innerHTML='Type: '+officer.type;
+      position1.innerHTML='Position: '+officer.position;
+      contact1.innerHTML='Contact: '+officer.contact;
+      whatsapp_contact1.innerHTML='Whatsapp number: '+officer.whatsapp_contact;
+      address1.innerHTML='Address: '+officer.address;
+      
+      newdiv.appendChild(pic);
+      newdiv1.appendChild(name1);
+      newdiv1.appendChild(gender1);
+      newdiv1.appendChild(type1);
+      newdiv1.appendChild(position1);
+      newdiv1.appendChild(contact1);
+      newdiv1.appendChild(whatsapp_contact1);
+      newdiv1.appendChild(address1);
+      newdiv.appendChild(newdiv1);
+
+      mydiv.appendChild(newdiv);
+      div.appendChild(mydiv);
+    })
+  }
+  else{
+    const para=document.getElementById('notFound');
+    para.innerHTML='Data not found!';
+    
+  }
+};
+document.getElementById('submit').addEventListener('click',details);
+  
+
+                                                                                                                                                                  
